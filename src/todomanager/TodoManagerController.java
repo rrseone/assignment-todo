b943822acdf002ad9eecfdf9376537a477b45517
Rubel Rashid <rrseucse@gmail.com>
2017-11-30 18:33:32 +0600

    Delete list task view and add scrolepane and checkedbox

--- src/todomanager/TodoManagerController.java
+++ src/todomanager/TodoManagerController.java
@@ -18,9 +18,11 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 
 /**
  *
@@ -33,9 +35,9 @@ public class TodoManagerController implements Initializable {
     @FXML
     private TextField newTodo;
     @FXML
-    private ListView<String> todoTaskOne;
+    private VBox todoTaskOne;
     @FXML
-    private ListView<String> todoTaskTwo;
+    private VBox todoTaskTwo;
     @FXML
     private TextField newTask;
     
@@ -43,9 +45,7 @@ public class TodoManagerController implements Initializable {
     ArrayList<Todo> allInfo = new ArrayList<>();
     ObservableList<String> todoArrayList;
     String selectedTitle;
-    ObservableList<String> selectedRunTask;
-    ObservableList<String> selectedEndTask;
-    
+    ArrayList<CheckBox> rt = new ArrayList<>();
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         try {
@@ -85,11 +85,31 @@ public class TodoManagerController implements Initializable {
             todoArrayList = FXCollections.observableArrayList(titleArray);
             todoList.setItems(todoArrayList);
             
-            selectedRunTask = FXCollections.observableArrayList(allInfo.get(0).getTaskRun());
-            todoTaskOne.setItems(selectedRunTask);
-            
-            selectedEndTask = FXCollections.observableArrayList(allInfo.get(0).getTaskEnd());
-            todoTaskTwo.setItems(selectedEndTask);
+            //selectedRunTask = FXCollections.observableArrayList(allInfo.get(0).getTaskRun());
+            ArrayList<String> srt = new ArrayList<>(allInfo.get(0).getTaskRun());
+            for(String s: srt){
+                CheckBox c = new CheckBox(s);
+                rt.add(c);
+                
+                /*c.setOnAction(click -> {
+                System.out.println(c.getText());
+                todoTaskTwo.getChildren().add(c);
+                });*/
+            }
+            todoTaskOne.getChildren().addAll(rt);
+            ArrayList<String> set = new ArrayList<>(allInfo.get(0).getTaskRun());
+            for(String s: set){
+                CheckBox c = new CheckBox(s);
+                c.setSelected(true);
+                todoTaskTwo.getChildren().add(c);
+                
+                c.setOnAction(click -> {
+                    System.out.println(c.getText());
+                    todoTaskOne.getChildren().add(c);
+                });
+            }
+            /*selectedEndTask = FXCollections.observableArrayList(allInfo.get(0).getTaskEnd());
+            todoTaskTwo.setItems(selectedEndTask);*/
         } catch (FileNotFoundException ex) {
             Logger.getLogger(TodoManagerController.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
@@ -143,11 +163,32 @@ public class TodoManagerController implements Initializable {
         selectedTitle = selectedTitle.substring(1, selectedTitle.length() - 1);
         for (Todo s: allInfo) {
             if(selectedTitle.equals(s.getTodoTitle())) {
-                ArrayList<String> t = new ArrayList<>(s.getTaskRun());
-                selectedRunTask = FXCollections.observableArrayList(s.getTaskRun());
+                ArrayList<String> srt = new ArrayList<>(s.getTaskRun());
+                todoTaskOne.getChildren().clear();
+                for(String d: srt){
+                    CheckBox c = new CheckBox(d);
+                    todoTaskOne.getChildren().add(c);
+                    
+                    c.setOnAction(click -> {
+                        System.out.println(c.getText());
+                        todoTaskTwo.getChildren().add(c);
+                    });
+                }
+                todoTaskTwo.getChildren().clear();
+                for(String d: srt){
+                    CheckBox c = new CheckBox(d);
+                    c.setSelected(true);
+                    todoTaskTwo.getChildren().add(c);
+                    
+                    c.setOnAction(click -> {
+                        System.out.println(c.getText());
+                        todoTaskOne.getChildren().add(c);
+                    });
+                }
+                /*selectedRunTask = FXCollections.observableArrayList(s.getTaskRun());
                 todoTaskOne.setItems(selectedRunTask);
                 selectedEndTask = FXCollections.observableArrayList(s.getTaskEnd());
-                todoTaskTwo.setItems(selectedEndTask);
+                todoTaskTwo.setItems(selectedEndTask);*/
                 break;
             }
         }
